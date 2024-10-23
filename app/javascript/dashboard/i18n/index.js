ac7fb3fcc7c11f646819204ca78ff95bd05098e0
KokeroO <kokero@hotmail.com>
2024-10-23 11:36:41 -0300

    Innitial o branch 1.x

---
+++ app/javascript/dashboard/i18n/index.js
@@ -0,0 +1,13 @@
+
+import en from './locale/en';
+import es from './locale/es';
+import pt_BR from './locale/pt_BR';
+import pt from './locale/pt';
+
+
+export default {
+  en,
+  es,
+  pt_BR,
+  pt,
+};
